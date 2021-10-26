363ef26915fa1b554d3b8cd1d5e0313f702ad4b3
Guixia-Z <a106907278@gmail.com>
2021-10-26 09:35:53 -0400

    add

--- admin.php
+++ admin.php
@@ -4,4 +4,10 @@ require_once 'vendor/autoload.php';
 
 require_once 'init.php';
 
+
+$app->get('/admin/waitinglist', function ($request, $response, $args) {
+    $waitingList = DB::query("SELECT * FROM waitinglist");
+    return $this->view->render($response, '/admin/waitinglist.html.twig', ['list' => $waitingList]);
+});
+
 // $app->get('/admin/user/list', function .....);
