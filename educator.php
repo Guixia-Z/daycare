fa84603e4032ce2d2daac1089d1f8755031ebf7a
LinLynne <lynnelin2019@gmail.com>
2021-10-29 11:40:12 -0400

    Update educator.php
    add

--- educator.php
+++ educator.php
@@ -8,18 +8,25 @@ $app->get('/educatoraccount', function ($request, $response, $args) {
     return $this->view->render($response, 'educatoraccount.html.twig');
 });
 
-$app->get('/childlist', function ($request, $response, $args) {
+$app->get('/educator/childlist', function ($request, $response, $args) {
     $educatorId = $_SESSION["user"]["id"];
-    $childList = DB::query("SELECT * FROM children WHERE educatorId=%i", $educatorId);
-    return $this->view->render($response, 'childlist.html.twig',
+    $childList = DB::query("SELECT * FROM children WHERE educatorId=%d", $educatorId);
+    return $this->view->render($response, '/educator/childlist.html.twig',
         ['list' => $childList]);
 });
 
 $app->get('/children_attenance', function ($request, $response, $args) {
     $today = date("Y/m/d");
     $educatorId = $_SESSION["user"]["id"];
-    $childList = DB::query("SELECT * FROM children WHERE educatorId=%i", $educatorId);
+    $childList = DB::query("SELECT * FROM children WHERE educatorId=%d", $educatorId);
     return $this->view->render($response, 'children_attenance.html.twig',
         ['list' => $childList, 'today' => $today]);
 });
 
+$app->get('/educator/addchildnotes', function ($request, $response, $args) {
+    $educatorId = $_SESSION["user"]["id"];
+    $childList = DB::query("SELECT * FROM children WHERE educatorId=%d", $educatorId);
+    return $this->view->render($response, '/educator/addchildnotes.html.twig',
+    ['list' => $childList  ]
+     );
+});
