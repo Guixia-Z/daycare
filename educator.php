45be00965ece790b6b0722d148aed94f92ae06c2
LinLynne <lynnelin2019@gmail.com>
2021-11-15 09:23:45 -0500

    modify
    modify

--- educator.php
+++ educator.php
@@ -4,16 +4,20 @@ require_once 'vendor/autoload.php';
 
 require_once 'init.php';
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+
 $app->get('/educator/educator_board', function ($request, $response, $args) {
     return $this->view->render($response, '/educator/educator_board.html.twig');
 });
-
-
 $app->get('/educator/childlist', function ($request, $response, $args) {
     $educatorId = $_SESSION["user"]["id"];
+    $educator = DB::queryFirstRow("SELECT concat(firstName,' ', lastName) AS fullName, concat(b.groupName,'-',b.groupDescription) AS groupAll FROM users a, groups b WHERE a.id=b.educatorId and a.id=%d", $educatorId);
     $childList = DB::query("SELECT * FROM children WHERE educatorId=%d", $educatorId);
+
     return $this->view->render($response, '/educator/childlist.html.twig',
-        ['list' => $childList]);
+['list' => $childList, 'educator'=>$educator]);
 });
 
   $app->get('/educator/addchildnotes', function ($request, $response, $args) {
@@ -33,11 +37,11 @@ $app->get('/educator/childlist', function ($request, $response, $args) {
     $app->get('/educator/note_remove', function ($request, $response, $args) {
         $noteId = $request->getParam('noteId');
         $childId = $request->getParam('childId');
-        $today = date("Y/m/d");
+        $today = date("Y-m-d");
         DB::delete("childnotes", "id=%i", $noteId);
         $List = DB::query("SELECT * FROM childnotes");
         $educatorId = $_SESSION["user"]["id"];
-        $noteList = DB::query("SELECT a.id, CONCAT(date(a.noteCreatedTS), (case time(a.noteCreatedTS) when '00:00:00' then '' else time(a.noteCreatedTS) end )) noteCreatedTS, a.weight, a.height, a.skills, a.note, a.photoFilePath, a.educatorId, a.childId, b.firstName,b.lastName FROM childnotes a,children b WHERE a.childId= b.id and a.educatorId =%d and a.childId=%d order by a.id desc" , $educatorId, $childId);
+        $noteList = DB::query("SELECT a.id,noteCreatedTS, a.weight, a.height, a.skills, a.note, a.photoFilePath, a.educatorId, a.childId, b.firstName,b.lastName FROM childnotes a,children b WHERE a.childId= b.id and a.educatorId =%d and a.childId=%d order by a.id desc" , $educatorId, $childId);
         return $this->view->render($response, '/educator/noteHistory.html.twig',
         ['list' => $noteList,'today' => $today, 'childId'=>$childId]);
         });
@@ -45,23 +49,36 @@ $app->get('/educator/childlist', function ($request, $response, $args) {
     $app->get('/educator/noteHistory', function ($request, $response, $args) {
         $childId = $request->getParam('id');
         $educatorId = $_SESSION["user"]["id"];
-        $today = date("Y/m/d");
-        $noteList = DB::query("SELECT a.id, CONCAT(date(a.noteCreatedTS), (case time(a.noteCreatedTS) when '00:00:00' then '' else time(a.noteCreatedTS) end )) noteCreatedTS, a.weight, a.height, a.skills, a.note, a.photoFilePath, a.educatorId, a.childId, b.firstName,b.lastName FROM childnotes a,children b WHERE a.childId= b.id and a.educatorId =%d and a.childId=%d order by a.id desc" , $educatorId, $childId);
+        $today = date("M d, Y");
+        $child = DB::queryFirstRow("SELECT a.firstName,  a.lastName FROM children a WHERE a.id=%d order by a.id desc" , $childId);
+        $noteList = DB::query("SELECT a.id,  noteCreatedTS, a.weight, a.height, a.skills, a.note, a.photoFilePath, a.educatorId, a.childId, b.firstName,b.lastName FROM childnotes a,children b WHERE a.childId= b.id and a.educatorId =%d and a.childId=%d order by a.id desc" , $educatorId, $childId);
+        foreach ($noteList as &$note) {
+            $datetime = strtotime($note['noteCreatedTS']);
+            $postedDate = date('M d, Y', $datetime);
+            $note['noteCreatedTS'] = $postedDate;
+            $fullBodyNoTags = strip_tags($note['note']);
+            $note['note'] = $fullBodyNoTags;
+        }
         return $this->view->render($response, '/educator/noteHistory.html.twig',
-        ['list' => $noteList,'today' => $today, 'childId'=>$childId]);
+        ['list' => $noteList,'today' => $today, 'childId'=>$childId,'child'=>$child]);
     });
 
     $app->get('/educator/attendanceHistory', function ($request, $response, $args) {
-        $today = date("Y/m/d");
+        $today = date("M d, Y");
         $childId = $request->getParam('id');
+        $child = DB::queryFirstRow("SELECT a.firstName,  a.lastName FROM children a WHERE a.id=%d order by a.id desc" , $childId);
         $attendanceList = DB::query("select c.id childId, a.id attendanceId, c.firstName, c.lastName, a.date, (CASE a.startTime WHEN '00:00:00' THEN '' ELSE a.startTime END) AS startTime,( CASE a.endTime WHEN '00:00:00' THEN '' ELSE a.endTime END ) AS endTime, a.note, a.status FROM children c, attendance a where a.childId=c.id and c.id=%d order by a.date desc", $childId);
+        foreach ($attendanceList as &$note) {
+            $fullBodyNoTags = strip_tags($note['note']);
+            $note['note'] = $fullBodyNoTags;
+        }
         return $this->view->render($response, '/educator/attendanceHistory.html.twig',
-        [ 'today' => $today,'list'=>$attendanceList,'childId'=>$childId]);
+        [ 'today' => $today,'list'=>$attendanceList,'childId'=>$childId,'child'=>$child]);
 
     });
 
     $app->get('/educator/attendance_remove', function ($request, $response, $args) {
-        $today = date("Y/m/d");
+        $today = date("M d, Y");
         $attendanceId = $request->getParam('attendanceId');
         $childId = $request->getParam('childId');
         DB::delete("attendance", "id=%i", $attendanceId);
@@ -73,11 +90,19 @@ $app->get('/educator/childlist', function ($request, $response, $args) {
 $app->get('/educator/childnotes_detail', function ($request, $response, $args) {
     $educatorId = $_SESSION["user"]["id"];
     $noteList = DB::query("SELECT a.id, CONCAT(date(a.noteCreatedTS), (case time(a.noteCreatedTS) when '00:00:00' then '' else time(a.noteCreatedTS) end )) noteCreatedTS, a.weight, a.height, a.skills, a.note, a.photoFilePath, a.educatorId, a.childId, b.firstName,b.lastName FROM childnotes a,children b WHERE a.childId= b.id and a.educatorId =%d order by a.id desc ", $educatorId);
+   
+     foreach ($noteList as &$note) {
+        $datetime = strtotime($note['noteCreatedTS']);
+        $postedDate = date('M d, Y', $datetime);
+        $note['noteCreatedTS'] = $postedDate;
+        $fullBodyNoTags = strip_tags($note['note']);
+        $note['note'] = $fullBodyNoTags;
+    }
     return $this->view->render($response, '/educator/childnotes_detail.html.twig',
     ['list' => $noteList]);});
     
     $app->get('/educator/child_attendance', function ($request, $response, $args) {
-        $today = date("Y/m/d");
+        $today = date("M d, Y");
         $id = $request->getParam('childId');
         $child = DB::queryFirstRow("select a.id, a.firstName, a.lastName from children a where a.id=%d", $id);
         return $this->view->render($response, '/educator/child_attendance.html.twig',
@@ -85,12 +110,21 @@ $app->get('/educator/childnotes_detail', function ($request, $response, $args) {
     });
     
 $app->get('/educator/child_attendance_detail', function ($request, $response, $args) {
-    $today = date("Y/m/d");
+   
+    $educatorId = $_SESSION["user"]["id"];
+    
+    //$attendanceList = DB::query("SELECT a.id,a.date, (CASE a.startTime WHEN '00:00:00' THEN '' ELSE a.startTime END) AS startTime,( CASE a.endTime WHEN '00:00:00' THEN '' ELSE a.endTime END ) AS endTime, a.status,a.note,a.childId, b.firstName,b.lastName FROM attendance a,children b WHERE a.childId = b.id and b.educatorId =%d order by a.date desc", $educatorId);
+    return $this->view->render($response, '/educator/child_attendance_detail.html.twig');
+    //['list' => $attendanceList]);
+
+});
+
+$app->get('/educator/child_attendance_detail_json', function (Request $request, Response $response, array $args) {
     $educatorId = $_SESSION["user"]["id"];
     $attendanceList = DB::query("SELECT a.id,a.date, (CASE a.startTime WHEN '00:00:00' THEN '' ELSE a.startTime END) AS startTime,( CASE a.endTime WHEN '00:00:00' THEN '' ELSE a.endTime END ) AS endTime, a.status,a.note,a.childId, b.firstName,b.lastName FROM attendance a,children b WHERE a.childId = b.id and b.educatorId =%d order by a.date desc", $educatorId);
-    return $this->view->render($response, '/educator/child_attendance_detail.html.twig',
-    ['list' => $attendanceList, 'today' => $today]);
-
+    $json = json_encode($attendanceList, JSON_PRETTY_PRINT);
+    $response->getBody()->write($json);
+    return $response;
 });
 
 
@@ -98,26 +132,41 @@ $app->post('/educator/child_attendance', function ($request, $response, $args) u
     $childId = $request->getParam('childId');
     $firstName = $request->getParam('firstName');
     $lastName = $request->getParam('lastName');
-    $date= $request->getParam('date');
-    $startTS = $request->getParam('startTime');
-    $endTS= $request->getParam('endTime');
+    $date=date('Y-m-d', strtotime($request->getParam('date'))); 
+    $startTS = date('h:i:sa',strtotime($request->getParam('startTime')));
+    $endTS= date('h:i:sa',strtotime($request->getParam('endTime')));
     $status= $request->getParam('status');
     $note = $request->getParam('note');
-    $errorList = [];
     $today = date("Y-m-d");
+    $errorList = [];
     
-    if($status == "absent"){
-        $valuesList = ["date" =>$date,"startTime" => "", "endTime" => "", "status" => "absent", "note" => $note, "childId"=>intval($childId)];
-        DB::insert('attendance', $valuesList);
-    }else{
-        $valuesList = ["date" =>$date,"startTime" => $startTS, "endTime" => $endTS, "status" => $status, "note" => $note, "childId"=>intval($childId)];
-        DB::insert('attendance', $valuesList);
+    if ($date=='1969-12-31') {
+        $errorList[] = "You must choose a date";
     }
-    
+    if ($startTS=='07:00:00pm'&&$endTS=='07:00:00pm'&&$status == "present") {
+        $errorList[] = "You must choose one time";
+    }
     if ($errorList) { 
-        return $this->view->render($response, '/educator/child_attendance', ['errorList' => $errorList, 'v' => $valuesList]);
+        $child = DB::queryFirstRow("select a.id, a.firstName, a.lastName from children a where a.id=%d", $childId);
+        return $this->view->render($response, 
+        '/educator/child_attendance.html.twig', 
+        ['errorList' => $errorList, 'id'=>$childId, 'today' => $today,'child'=>$child]);
     } else { 
-    return $this->view->render($response, '/educator/child_check_success.html.twig');}
+        if ($startTS=='07:00:00pm'){
+            $startTS='00:00:00'; 
+        }
+        if ($endTS=='07:00:00pm'){
+            $endTS='00:00:00'; 
+        }
+        if($status == "absent"){
+            $valuesList = ["date" =>$date,"startTime" => "", "endTime" => "", "status" => "absent", "note" => $note, "childId"=>intval($childId)];
+            DB::insert('attendance', $valuesList);
+        }else{
+            $valuesList = ["date" =>$date,"startTime" => $startTS, "endTime" => $endTS, "status" => $status, "note" => $note, "childId"=>intval($childId)];
+            DB::insert('attendance', $valuesList);
+        }
+        return $this->view->render($response, '/educator/child_check_success.html.twig',['childId'=>$childId]);
+    }
 });
 
 
@@ -133,7 +182,7 @@ $app->post('/educator/noteHistory/delete/{id:[0-9]+}', function ($request, $resp
 
 
 $app->get('/educator/child_attendance_check_success', function ($request, $response, $args) {
-    $today = date("Y/m/d");
+    $today = date("M d, Y");
     $id = $request->getParam('id');
     $childList = DB::query("select a.id, a.firstName, a.lastName from children a where a.id=%d", $id);
     return $this->view->render($response, '/educator/child_attendance_check_success.html.twig',
--- utils.php
+++ utils.php
@@ -22,36 +22,60 @@ $app->post('/educator/addchildnotes', function ($request, $response, $args) use 
     if (!is_numeric($height)||$height  < 0 ) {
         $errorList[] = "height can not be empty,must be a number positive";
     }
-    // image validation
+   
     $uploadedImage = $request->getUploadedFiles()['image'];
-    if($_FILES['image']['name'] != "") {
-        $destImageFilePath = null;
-        $result = verifyUploadedPhoto($uploadedImage, $destImageFilePath);
-        if ($result !== TRUE) {
-            $errorList []= $result;
-        }
-    }
-    if ($errorList) { 
-        $child = $request->getParam('childId');
-        $child = DB::queryFirstRow("select a.* from children a where a.id=%d", $childId);
-        return $this->view->render($response, '/educator/addchildnotes.html.twig',
-         ['errorList' => $errorList, 'child' => $child]);
+    $destImageFilePath = null;
+    $result = verifyUploadedPhoto($uploadedImage, $destImageFilePath);
+    if ($result !== TRUE) {
+        $errorList []= $result;
     }
     $valuesList = ["noteCreatedTS" => $noteCreatedTs, 
-        "weight" => floatval ($weight), 
-        "height" => floatval ($height), 
-        "skills" => $skills, 
-        "note"=>$note,
-        "educatorId" => intval($educatorId),
-        "childId" => intval($childId)];
-    if($_FILES['image']['name'] != "") {
+    "weight" => floatval ($weight), 
+    "height" => floatval ($height), 
+    "skills" => $skills, 
+    "note"=>$note,
+    "educatorId" => intval($educatorId),
+    "childId" => intval($childId)];
+    if ($errorList) {
+        $child = $request->getParam('childId');
+        $child = DB::queryFirstRow("select a.* from children a where a.id=%i", $childId);
+        return $this->view->render($response, '/educator/addchildnotes.html.twig',
+         ['errorList' => $errorList, 'child' => $child,'v' => $valuesList]);
+
+    } else { 
         $uploadedImage->moveTo($destImageFilePath); // FIXME: check if it failed !
         $valuesList['photoFilePath'] = $destImageFilePath;
+        
+       
+        DB::insert('childnotes', $valuesList);
+        return $this->view->render($response, '/educator/notesave_success.html.twig',['childId'=>$childId]);
     }
-    DB::insert('childnotes', $valuesList);
-    return $this->view->render($response, '/educator/notesave_success.html.twig');
+    
 });
 
+function verifyUploadedPhoto($photo, &$newFilePath) {
+    if ($photo->getError() !== UPLOAD_ERR_OK) {
+        return "Error uploading photo " . $photo->getError();
+    }
+    if($photo->getSize() > 2*1024*1024){
+        return "File too big. 2MB max is allowed.";
+    }
+    $info = getimagesize($photo->file);
+    if ($info[0] < 100 || $info[0] > 1000 || $info[1] < 100 || $info[1] > 1000) {
+        return "Width and height must be within 100-1000 pixels range";
+    }
+    $ext = "";
+    switch ($info['mime']) {
+        case 'image/jpeg': $ext = "jpg"; break;
+        case 'image/gif': $ext = "gif"; break;
+        case 'image/png': $ext = "png"; break;
+        case 'image/bmp': $ext = "bmp"; break;
+        default:
+            return "Only JPG, GIF, and PNG file types are accepted";
+    }
+    $newFilePath = "uploads/" . substr($photo->getClientFilename(),0,strpos($photo->getClientFilename(), '.')) . "." . $ext;
+    return TRUE;
+}
 
 $app->post('/educator/note_edit', function ($request, $response, $args) use ($log) {
     $id = $request->getParam('id');
@@ -94,33 +118,15 @@ $app->post('/educator/note_edit', function ($request, $response, $args) use ($lo
         $uploadedImage->moveTo($destImageFilePath); // FIXME: check if it failed !
         $valuesList['photoFilePath'] = $destImageFilePath;
     }
-    DB::update('childnotes', $valuesList, 'id=%d', intval($id));
+    DB::update('childnotes', $valuesList, 'id=%i', intval($id));
     return $this->view->render($response, '/educator/editsave_success.html.twig');
 });
 
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
 
-
-
-function verifyUploadedPhoto($photo, &$newFilePath) {
-    if ($photo->getError() !== UPLOAD_ERR_OK) {
-        return "Error uploading photo " . $photo->getError();
-    }
-    if($photo->getSize() > 2*1024*1024){
-        return "File too big. 2MB max is allowed.";
-    }
-    $info = getimagesize($photo->file);
-    if ($info[0] < 100 || $info[0] > 1000 || $info[1] < 100 || $info[1] > 1000) {
-        return "Width and height must be within 100-1000 pixels range";
-    }
-    $ext = "";
-    switch ($info['mime']) {
-        case 'image/jpeg': $ext = "jpg"; break;
-        case 'image/gif': $ext = "gif"; break;
-        case 'image/png': $ext = "png"; break;
-        case 'image/bmp': $ext = "bmp"; break;
-        default:
-            return "Only JPG, GIF, and PNG file types are accepted";
-    }
-    $newFilePath = "uploads/" . substr($photo->getClientFilename(),0,strpos($photo->getClientFilename(), '.')) . "." . $ext;
-    return TRUE;
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
 }
+
